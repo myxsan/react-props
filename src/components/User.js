e7a98f2de074168fddfc7d7aa3408fde95f30882
myxsan <yksl3461@gmail.com>
2023-01-14 07:04:29 +0300

    Deafult props

--- src/components/User.js
+++ src/components/User.js
@@ -2,12 +2,14 @@ import userEvent from "@testing-library/user-event";
 import PropTypes from "prop-types";
 
 function User({ name, surName, age, isLoggedIn, friends, address }) {
+  if (!isLoggedIn) return(
+    <div>{"You did not signed in !!!"}</div>
+  );
+
   return (
     <>
       <div>
-        {isLoggedIn
-          ? `Hello ${name} ${surName} (${age})`
-          : "You did not signed in!"}
+        {`Hello ${name} ${surName} (${age})`}
       </div>
       {friends.map((friend) => (
         <div key={friend.id}>{friend.name}</div>
@@ -26,8 +28,15 @@ User.propTypes = {
   friends: PropTypes.array,
   address: PropTypes.shape({
     title: PropTypes.string,
-    zipCode: PropTypes.number
-})
+    zipCode: PropTypes.number,
+  }),
+};
+
+User.defaultProps = {
+  isLoggedIn: false,
+  name: "name",
+  surname: "surname",
+  age: "age",
 };
 
 export default User;
